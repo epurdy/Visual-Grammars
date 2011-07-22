933d6f9dbf88a84d2715704c0db183c0e25cdd8c
Eric Purdy <epurdy@uchicago.edu>
2011-07-22 01:30:33 -0500

    slight modification to the guessing method

--- bin/guess_annotation.ml
+++ bin/guess_annotation.ml
@@ -64,31 +64,61 @@ let match_annotations ann1 ann2 =
 (* 	(xrank2 >> i) (yrank2 >> i); *)
 (*     done; *)
 
-    for pass=1 to n*n*n do
+    let anychange = ref true in
+    let sq x = x*x in
+    
+    while !anychange do
+      anychange := false;
 (*       printf "pass #%d\n" pass; *)
     for a=1 to n do
       for b=1 to n do
 	let c, d = themap >> a, themap >> b in
 
-	let axr, ayr = xrank1 >> a, yrank1 >> a in
-	let bxr, byr = xrank1 >> b, yrank1 >> b in
-	let cxr, cyr = xrank2 >> c, yrank2 >> c in
-	let dxr, dyr = xrank2 >> d, yrank2 >> d in
+	let ax,ay = rann1 >> a in
+	let bx,by = rann1 >> b in
+	let cx,cy = rann2 >> c in
+	let dx,dy = rann2 >> d in
 
-	let tx1old, ty1old = axr-cxr, ayr-cyr in
-	let tx2old, ty2old = bxr-dxr, byr-dyr in
-	let tx1new, ty1new = axr-dxr, ayr-dyr in
-	let tx2new, ty2new = bxr-cxr, byr-cyr in
-(* 	let cost = tx1old*tx1old + ty1old*ty1old + tx2old*tx2old + ty2old*ty2old in *)
-(* 	let newcost = tx1new*tx1new + ty1new*ty1new + tx2new*tx2new + ty2new*ty2new in *)
-	let cost = (abs tx1old) + (abs ty1old) + (abs tx2old) + (abs ty2old) in
-	let newcost = (abs tx1new) + (abs ty1new) + (abs tx2new) + (abs ty2new) in
+	let ac2 = float_of_int ((sq (ax-cx)) + (sq (ay-cy))) in
+	let bd2 = float_of_int ((sq (bx-dx)) + (sq (by-dy))) in
+	let ad2 = float_of_int ((sq (ax-dx)) + (sq (ay-dy))) in
+	let bc2 = float_of_int ((sq (bx-cx)) + (sq (by-cy))) in
 
-(* 	  printf "trying %d and %d, %d vs %d\n" c d newcost cost; *)
+	let ac = sqrt ac2 in
+	let bd = sqrt bd2 in
+	let ad = sqrt ad2 in
+	let bc = sqrt bc2 in
+
+	let downweight = 100. in
+
+	(* using only squared distance makes the search too
+	   non-convex, using only linear distance causes it to not
+	   care about some switches *)
+
+	let cost = ac +. bd +. (ac2 +. bd2) /. downweight in
+	let newcost = ad +. bc +. (ad2 +. bc2) /. downweight  in
+
+	  (* 	let axr, ayr = xrank1 >> a, yrank1 >> a in *)
+(* 	let bxr, byr = xrank1 >> b, yrank1 >> b in *)
+(* 	let cxr, cyr = xrank2 >> c, yrank2 >> c in *)
+(* 	let dxr, dyr = xrank2 >> d, yrank2 >> d in *)
+
+(* 	let tx1old, ty1old = axr-cxr, ayr-cyr in *)
+(* 	let tx2old, ty2old = bxr-dxr, byr-dyr in *)
+(* 	let tx1new, ty1new = axr-dxr, ayr-dyr in *)
+(* 	let tx2new, ty2new = bxr-cxr, byr-cyr in *)
+(* 	let cost = (abs tx1old) + (abs ty1old) + (abs tx2old) + (abs ty2old) in *)
+(* 	let newcost = (abs tx1new) + (abs ty1new) + (abs tx2new) + (abs ty2new) in *)
+
 	  if newcost < cost then begin 
+	    anychange := true;
 	    themap << (a,d);
 	    themap << (b,c);
 (* 	    printf "swapped %d and %d, %d < %d\n" c d newcost cost; *)
+(* 	    printf "point #%d = (%d,%d), which is more like (%d,%d) (dist %d) than (%d,%d) (dist %d)\n" c cx cy bx by bc2 ax ay ac2; *)
+(* 	    printf "point #%d = (%d,%d), which is more like (%d,%d) (dist %d) than (%d,%d) (dist %d)\n" d dx dy ax ay ad2 bx by bd2; *)
+(* 	    printf "---\n"; *)
+
 (* 	    printf "point #%d had ranks %d,%d, which is more like %d,%d\n" c cxr cyr bxr byr; *)
 (* 	    printf "point #%d had ranks %d,%d, which is more like %d,%d\n" d dxr dyr axr ayr; *)
 (* 	    printf "---\n"; *)
@@ -96,6 +126,9 @@ let match_annotations ann1 ann2 =
       done
     done;
     done;
+
+(*       printf "about to suicide =}\n%!"; *)
+(*       assert(0 == 1); *)
 
     let foo= mkhash 100 in
     Hashtbl.iter 
