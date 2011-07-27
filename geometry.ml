4841401330948acb13914edb5913b4a74e741d36
Eric Purdy <epurdy@uchicago.edu>
2011-07-27 15:29:01 -0500

    more succinct (Complex -> C)

--- geometry.ml
+++ geometry.ml
@@ -1,12 +1,14 @@
 open Util.Cops
 open Printf
 
+module C = Complex
+
 type ipt = int * int
 
-type cpt = Complex.t
+type cpt = C.t
 
 let string_of_cpt p =
-  sprintf "(%f,%f)" p.Complex.re p.Complex.im
+  sprintf "(%f,%f)" p.C.re p.C.im
 
 let print_cpt p = 
   printf "%s\n%!" (string_of_cpt p)
@@ -15,34 +17,34 @@ let print_cpt p =
     ------------------------------------------------------------- *)
 
 let flip_x z =
-  {Complex.re = -. z.Complex.re;
-   Complex.im = z.Complex.im}
+  {C.re = -. z.C.re;
+   C.im = z.C.im}
 
 let flip_xy z = 
-  {Complex.re = z.Complex.im;
-   Complex.im = z.Complex.re}
+  {C.re = z.C.im;
+   C.im = z.C.re}
 
 (* multiply complex number by scalar *)
 let scale x s = 
-  { Complex.re = x.Complex.re *. s;
-    Complex.im = x.Complex.im *. s }
+  { C.re = x.C.re *. s;
+    C.im = x.C.im *. s }
 
 (* dot product between vectors represented as complex numbers *)  
 let dot x y = 
-  x.Complex.re *. y.Complex.re +. x.Complex.im *. y.Complex.im
+  x.C.re *. y.C.re +. x.C.im *. y.C.im
 
 let dist2 p q = 
-  Complex.norm2 (p -& q)
+  C.norm2 (p -& q)
 
 let linecostfn (curve : cpt array) (bg : int) (en : int) : float =
   let p = curve.(bg) in
   let q = curve.(en) in
   let pq = q -& p in
-  let upq = scale pq (1.0 /. Complex.norm pq) in
+  let upq = scale pq (1.0 /. C.norm pq) in
   let tot = ref 0. in
     for i=bg to en do
       let z = curve.(i) in
-      let displacement = ((z -& p) /& upq).Complex.im in
+      let displacement = ((z -& p) /& upq).C.im in
 	tot := !tot +. displacement *. displacement;
     done;
     !tot
@@ -50,17 +52,17 @@ let linecostfn (curve : cpt array) (bg : int) (en : int) : float =
 (** Bookstein coordinates! *)
 
 let normalize (p, q, r) = 
-  let pnorm2 = Complex.norm2 p in
-  let qnorm2 = Complex.norm2 q in
-  let rnorm2 = Complex.norm2 r in
+  let pnorm2 = C.norm2 p in
+  let qnorm2 = C.norm2 q in
+  let rnorm2 = C.norm2 r in
   let size = 1.0 /. sqrt (pnorm2 +. qnorm2 +. rnorm2) in
     (scale p size,
      scale q size,
      scale r size)
 
 let normalize2 p q = 
-  let pnorm2 = Complex.norm2 p in
-  let qnorm2 = Complex.norm2 q in
+  let pnorm2 = C.norm2 p in
+  let qnorm2 = C.norm2 q in
   let size = 1.0 /. sqrt (pnorm2 +. qnorm2) in
     (scale p size,
      scale q size)
@@ -82,11 +84,11 @@ let center_and_scale (p, q, r) =
 
 (* complex representation of point *) 
 let complex_of_point (x, y) =
-  { Complex.re = float_of_int x; Complex.im = float_of_int y }
+  { C.re = float_of_int x; C.im = float_of_int y }
 
 (* point representation of complex number, should round! *)
 let point_of_complex p =
-  (int_of_float p.Complex.re, int_of_float p.Complex.im)
+  (int_of_float p.C.re, int_of_float p.C.im)
 
 
 
